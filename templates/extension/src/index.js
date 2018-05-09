fcc10adbbcbb52238e31bbda8cb6c0c06373761c
Connor Bär <connor.baer@sumup.com>
2018-05-09 09:59:03 +0200

    Add script to bootstrap new extensions

---
+++ templates/extension/src/index.js
@@ -0,0 +1,45 @@
+// When UI Extensions SDK is loaded the callback will be executed.
+window.contentfulExtension.init(initExtension);
+
+function initExtension(extensionsApi) {
+  // "extensionsApi" is providing an interface documented here:
+  // https://github.com/contentful/ui-extensions-sdk/blob/master/docs/ui-extensions-sdk-frontend.md
+
+  // Automatically adjust UI Extension size in the Web App.
+  extensionsApi.window.updateHeight();
+  extensionsApi.window.startAutoResizer();
+
+  const inputEl = document.getElementById('extension-input');
+
+  //  The field this UI Extension is assigned to.
+  const { field } = extensionsApi;
+
+  // Callback for changes of the field value.
+  const detachValueChangeHandler = field.onValueChanged(valueChangeHandler);
+  // Handle keyboard input.
+  inputEl.addEventListener('input', keyboardInputHandler);
+  // Handle DOM "onbeforeunload" event.
+  window.addEventListener('onbeforeunload', unloadHandler);
+
+  // Handler for external field value changes (e.g. when multiple authors are working on the same entry).
+  function valueChangeHandler(value) {
+    inputEl.value = value || '';
+  }
+
+  // Event handler for keyboard input.
+  function keyboardInputHandler() {
+    const { value } = inputEl;
+    if (typeof value !== 'string' || value === '') {
+      field.removeValue();
+    } else {
+      field.setValue(value);
+    }
+  }
+
+  // Event handler for window unload.
+  function unloadHandler() {
+    window.removeEventListener('onbeforeunload', unloadHandler);
+    inputEl.removeEventListener('input', keyboardInputHandler);
+    detachValueChangeHandler();
+  }
+}
